c72d74a15b187c83904d87d8934ea195743951d1
Cozma Rares <cozmarares11@gmail.com>
2023-08-20 20:44:33 +0300

    feat(home): add hero section

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,10 +3,13 @@ import GitHub from "../svg/GitHub";
 import Instagram from "../svg/Ingtagram";
 import Logo from "../svg/Logo";
 
+import heroBG from "../assets/background.mp4";
+
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 };
@@ -34,6 +37,28 @@ const NavBar = () => (
       </ul>
     </div>
   </aside>
+);
+
+const Hero = () => (
+  <section>
+    <video
+      loop
+      muted
+      autoPlay
+    >
+      <source
+        src={heroBG}
+        type="video/mp4"
+      />
+    </video>
+    <div></div>
+    <div>
+      <p>Enjoy your beer!</p>
+      <h1>beer boutique</h1>
+      <p>The best beer comes with the best foods!</p>
+      <button>reserve a table</button>
+    </div>
+  </section>
 );
 
 export default App;
