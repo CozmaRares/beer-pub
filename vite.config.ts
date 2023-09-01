951cf738038877c8089b78f15922156ac1c0ab9d
Cozma Rares <cozmarares11@gmail.com>
2023-09-01 20:01:08 +0300

    chore: add image optimisation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
