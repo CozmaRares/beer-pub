23f97af6ee2c5ad31d10354353d9b4637ea2031d
Cozma Rares <cozmarares11@gmail.com>
2023-09-01 20:25:28 +0300

    refactor(reports): run lighthouse for multiple paths

--- report.sh
+++ report.sh
@@ -5,11 +5,19 @@ CHROME_PATH="" # path to Chrome/Chromium executable
 ####################################
 
 # don't change anything below, or change at your own risk
+routes=("" "about" "menu" "blog" "contact")
 current_date=$(date +"%Y-%m-%d-%T")
 port=""
 
 function report(){
-    CHROME_PATH=$CHROME_PATH lighthouse "http://localhost:${port}/" --output html --output-path "reports/${current_date}.html" --view --chrome-flags="--headless"
+    local route="$1"
+    CHROME_PATH=$CHROME_PATH lighthouse "http://localhost:${port}/#/${route}" --output html --output-path "reports/${route}-${current_date}.html" --view --chrome-flags="--headless"
+}
+
+function generate_reports(){
+    for element in "${routes[@]}"; do
+        report "$element"
+    done
 }
 
 type lighthouse >/dev/null 2>&1 || { echo >&2 "lighthouse not found"; echo >&2 "See https://github.com/GoogleChrome/lighthouse#using-the-node-cli for installation."; exit 1; }
@@ -22,14 +30,14 @@ while getopts 'p:' flag; do
     case "${flag}" in
         p)
             port="${OPTARG}"
-            report
+            generate_reports
             exit
             ;;
         *)
             echo "Unknown flag: ${flag}"
-            exit 1
             ;;
     esac
 done
 
 echo "USAGE: ./report.sh -p <port_number>"
+exit 1
